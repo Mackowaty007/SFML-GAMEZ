30c36249905b9b76c5fb12475e3454a84a47f692
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2021-09-26 14:58:29 +0200

    Add files via upload

--- SFML-PerlinNoise2/main.cpp
+++ SFML-PerlinNoise2/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <math.h>
 
-#define GRID_S 10
+#define GRID_S 50
 #define SCREEN_H 1000
 #define SCREEN_W 1000
 #define PI 3.14159265
@@ -46,26 +46,29 @@ int main()
             float VectorGDL[2] = {cos(map[x/GRID_S][y/GRID_S+1] * PI / 180),sin(map[x/GRID_S][y/GRID_S+1] * PI / 180)};
             float VectorGDR[2] = {cos(map[x/GRID_S+1][y/GRID_S+1] * PI / 180),sin(map[x/GRID_S+1][y/GRID_S+1] * PI / 180)};
 
-            float VectorPX = (x-(x/GRID_S*GRID_S))/(GRID_S/2*sqrt(2));
-            float VectorPY = (y-(y/GRID_S*GRID_S))/(GRID_S/2*sqrt(2));
+            float VectorPUL[2] = {(x-(x/GRID_S*GRID_S))/(GRID_S/2*sqrt(2)),(y-(y/GRID_S*GRID_S))/(GRID_S/2*sqrt(2))};
+            float VectorPUR[2] = {((x-(x/GRID_S*GRID_S))-1)/(GRID_S/2*sqrt(2)),(y-(y/GRID_S*GRID_S))/(GRID_S/2*sqrt(2))};
+            float VectorPDL[2] = {(x-(x/GRID_S*GRID_S))/(GRID_S/2*sqrt(2)),((y-(y/GRID_S*GRID_S))-1)/(GRID_S/2*sqrt(2))};
+            float VectorPDR[2] = {((x-(x/GRID_S*GRID_S))-1)/(GRID_S/2*sqrt(2)),((y-(y/GRID_S*GRID_S))-1)/(GRID_S/2*sqrt(2))};
 
-            float dotProductUL = (VectorGUL[0] * VectorPX + VectorGUL[1] * VectorPY)*64+127;
-            float dotProductDL = (VectorGDL[0] * VectorPX + VectorGDL[1] * VectorPY)*64+127;
-            float dotProductUR = (VectorGUR[0] * VectorPX + VectorGUR[1] * VectorPY)*64+127;
-            float dotProductDR = (VectorGDR[0] * VectorPX + VectorGDR[1] * VectorPY)*64+127;
+            float dotProductUL = (VectorGUL[0] * VectorPUL[0] + VectorGUL[1] * VectorPUL[1])*64+127;
+            float dotProductDL = (VectorGDL[0] * VectorPDL[0] + VectorGDL[1] * VectorPDL[1])*64+127;
+            float dotProductUR = (VectorGUR[0] * VectorPUR[0] + VectorGUR[1] * VectorPUR[1])*64+127;
+            float dotProductDR = (VectorGDR[0] * VectorPDR[0] + VectorGDR[1] * VectorPDR[1])*64+127;
 
             //lerp(dotProductUL,dotProductUR,static_cast<float>(x-(x/GRID_S*GRID_S))/GRID_S);
 
+            int PixColor = slerp(slerp(dotProductUL,dotProductUR,static_cast<float>(x-(x/GRID_S*GRID_S))/GRID_S) , slerp(dotProductDL,dotProductDR,static_cast<float>(x-(x/GRID_S*GRID_S))/GRID_S) , static_cast<float>(y-(y/GRID_S*GRID_S))/GRID_S);
 
-            int PixColor = dotProductUL + dotProductDL + dotProductUR + dotProductDR;
+            //int PixColor = dotProductUL + dotProductDL + dotProductUR + dotProductDR;
 
-            PixColor = PixColor/4;
+            //PixColor = PixColor/4;
 
             pix.setFillColor(sf::Color(PixColor,PixColor,PixColor));
             app.draw(pix);
         }
     }
-    /*
+/*
     //draw the lines
     for(int x=0;x<SCREEN_W/GRID_S+1;x++){
         for(int y=0;y<SCREEN_H/GRID_S+1;y++){
@@ -73,8 +76,8 @@ int main()
             lines[1].position = sf::Vector2f(x*GRID_S+cos(map[x][y] * PI / 180)*GRID_S/2,y*GRID_S+sin(map[x][y] * PI / 180)*GRID_S/2);
             app.draw(lines);
         }
-    }*/
-
+    }
+*/
     app.display();
 
 	// Start the game loop
